5baf2eddcda9f75593fc3f40728dd4c48675f601
hv98 <hv98@global.tesco.org>
2019-04-19 14:38:25 +0530

    Modified view page

--- routes/index.js
+++ routes/index.js
@@ -20,34 +20,38 @@ router.post('/upload', upload.array('HtmlFile'), (req, res, next) =>
             "fileName": originalFileName,
             "htmlContent": htmltext
         });
-        HtmlFileSchema.find({ "fileName": originalFileName }, (err, response) =>
-        {
-            if (err) res.json("Error fetching data")
-            else {
-                if (response.length > 0) {
-                    res.status(200).send(`File is already exists in the name : ${originalFileName} | Please upload new file`);
-                } else {
-                    htmlFileData.save()
-                        .then(item =>
-                        {
-                            console.log("Response from DB : ", item);
-                            return item;
-                        })
-                        .catch(err =>
-                        {
-                            console.log("error occured while storing file into DB : ", err);
-                            res.status(400).send("unable to save to database");
-                        }
-                        );
+        try {
+            HtmlFileSchema.find({ "fileName": originalFileName }, (err, response) =>
+            {
+                if (err) res.json("Error fetching data")
+                else {
+                    console.log("response :: ", response);
+                    if (response.length > 0) {
+                        res.status(200).send(`File is already exists in the name : ${originalFileName} | Please upload new file`);
+                    } else {
+                        htmlFileData.save()
+                            .then(item =>
+                            {
+                                console.log("Response from DB : ", item);
+                                res.status(200).send('Html file content is successfully saved to database');
+                            })
+                            .catch(err =>
+                            {
+                                console.log("error occured while storing file into DB : ", err);
+                                res.status(400).send("unable to save to database");
+                            }
+                            );
+                    }
                 }
-            }
-        })
+            });
+        } catch (error) {
+            console.log("Error occured : ", error);
+        }
     });
-    res.status(200).send('Html file content is successfully saved to database');
 });
 
-//Basically this is an PUT API call. Since plane Html doesn't support PUT, using POST here for temporary fix
-router.post('/modifyFileContent', function (req, res)
+//Ideally this should be an PUT API call. Since plane Html doesn't support PUT, using POST here for temporary fix
+router.post('/modifyFileContent', (req, res) =>
 {
     let ModifiedContent = req.query.content;
     let fileId = req.query.fileName;
@@ -55,50 +59,54 @@ router.post('/modifyFileContent', function (req, res)
 
     HtmlFileSchema.updateOne({ "fileName": fileId }, updatedQuery, (err, response) =>
     {
-        console.log(response);
         if (err) res.json("Error fetching data", err);
         else {
-            console.log("update response : ", response);
-            //let fileArr = response[0].htmlContent;
-            res.status(200).send("successfully updates");
+            res.status(200).send("Successfully updated!!!");
         }
     });
 });
 
-router.get('/listFiles', function (req, res)
+router.get('/listFiles', (req, res) =>
 {
     let pageNo = parseInt(req.query.pageNo) || 1;
     let size = parseInt(req.query.size) || 10;
     let query = {}
     if (pageNo < 0 || pageNo === 0) {
-        return res.json("invalid page number, should start with 1")
+        return res.json("Invalid page number, should start with 1")
     }
     query.skip = size * (pageNo - 1)
     query.limit = size;
 
-    HtmlFileSchema.find({}, {}, query, (err, response) =>
-    {
-        if (err) res.json("Error fetching data", err);
-        else {
-            let fileArr = response.map(eachFile =>
-            {
-                return {
-                    "fileName": eachFile.fileName,
-                    "htmlContent": eachFile.htmlContent
-                }
-            });
-            res.status(200).send(fileArr);
-        }
-    });   
+    try {
+        HtmlFileSchema.find({}, {}, query, (err, response) =>
+        {
+            if (err)
+                res.json("Error fetching data", err);
+            else {
+                let fileArr = response.map(eachFile =>
+                {
+                    return {
+                        "fileName": eachFile.fileName,
+                        "htmlContent": eachFile.htmlContent
+                    }
+                });
+                res.status(200).send(fileArr);
+            }
+        }); 
+    } catch (error) {
+        res.send(error);
+    }
+       
 });
 
-router.get('/getFileByName', function (req, res)
+router.get('/getFileByName', (req, res) =>
 {
     let file = req.query.fileName;
 
     HtmlFileSchema.find({ "fileName": file }, (err, response) =>
     {
-        if (err) res.json("Error fetching data", err);
+        if (err)
+            res.json("Error fetching data", err);
         else {
             let fileArr = response[0].htmlContent;
             res.status(200).send(fileArr.toString());
